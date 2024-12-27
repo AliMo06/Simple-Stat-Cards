b7736a23d441df07a901fe2b3b10330970f5c051
AliMo06 <108025201+AliMo06@users.noreply.github.com>
2024-12-26 20:13:37 -0500

    inital setups pt 1

---
+++ ProjectCSH/ProjectCSH/SimpleStatCards.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using SimpleStatCards.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+
+namespace SSC
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class MyModName : BaseUnityPlugin
+    {
+        private const string ModId = "com.AliMo06.rounds.SimpleStatCards";
+        private const string ModName = "SimpleStatCards";
+        public const string Version = "1.0.0"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "SSC";
+        public static SimpleStatCards instance { get; private set; }
+
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            instance = this;
+            CustomCard.BuildCard<MyCardName>();
+        }
+    }
+}
